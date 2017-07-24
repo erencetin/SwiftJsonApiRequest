6500b8111932dab6d8a18b2e44af38aea5526a65
erencetin <eecetin@gmail.com>
2017-07-24 23:38:14 +0300

    ok

--- JsonApiSample/ViewController.swift
+++ JsonApiSample/ViewController.swift
@@ -13,6 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let urlString = "http://api.openweathermap.org/data/2.5/weather?q=umraniye&appid=9803d867cabd29b864efc9f3018efa16&units=metric"
+        //let urlString = "http://bucayapimarket.com/json.php"
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!){
+            (data,response,error) in
+           
+            if error != nil {
+                print(error!)
+            }
+            else
+            {
+                do{
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! NSArray
+                    print(json)
+                }
+                catch{
+                    print(error)
+                }
+            }
+            
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
